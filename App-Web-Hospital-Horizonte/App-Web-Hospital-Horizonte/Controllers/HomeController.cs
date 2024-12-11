9281ea61c7b2cc78e29ff641fb4b6d778108115b
sebasp-125 <sp6027193@gmail.com>
2024-12-10 18:28:08 -0600

    Update project

---
+++ App-Web-Hospital-Horizonte/App-Web-Hospital-Horizonte/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using App_Web_Hospital_Horizonte.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace App_Web_Hospital_Horizonte.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        //Loging
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ App-Web-Hospital-Horizonte/App-Web-Hospital-Horizonte/Models/InformationUser.cs
@@ -0,0 +1,6 @@
+public class InformationUser {
+    public int idUsuario {  get; set; }
+    public string NombreUsuarioI { get; set; }
+    public string PasswordUsuarioI { get; set; }
+    public string UrlImageProfile { get; set; }
+}
