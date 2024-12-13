9a86329e9b99515fcc96660881ee3bb5b40b0075
Juan Sebastián Montaño Pérez <134357858+sebasp-125@users.noreply.github.com>
2024-12-13 16:23:47 -0600

    Delete App-Web-Hospital-Horizonte directory

--- App-Web-Hospital-Horizonte/App-Web-Hospital-Horizonte/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace App_Web_Hospital_Horizonte.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
